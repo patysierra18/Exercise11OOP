c420eba0aa46791f7646060bf0fb076a82b26a0d
Paty <patysierra1@gmail.com>
2019-10-16 13:25:49 -0500

    First example of functions in java

---
+++ src/com/exercise11oop/model/Perro.java
@@ -0,0 +1,20 @@
+package com.exercise11oop.model;
+
+public class Perro 
+{
+	public int id;
+	private String name;
+	protected String raza;
+	public int age;
+	public double height;
+	
+	public Perro(int id, String name, String breed, int age, double height)
+	{
+		
+	}
+	public Perro() {}
+	
+	
+	
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Exercise11OOP {
+}
